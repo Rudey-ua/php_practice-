be3b24d9432762c55194b4f140f6b17346679e48
Rudey-ru <maksym.kostenko1@nure.ua>
2022-02-04 21:09:30 +0200

    second task done

--- index.php
+++ index.php
@@ -19,7 +19,6 @@
     </head>
     <body class="mod-bg-1 mod-nav-link ">
         <main id="js-page-content" role="main" class="page-content">
-
             <div class="col-md-6">
                 <div id="panel-1" class="panel">
                     <div class="panel-hdr">
@@ -31,59 +30,36 @@
                             <button class="btn btn-panel waves-effect waves-themed" data-action="panel-fullscreen" data-toggle="tooltip" data-offset="0,10" data-original-title="Fullscreen"></button>
                         </div>
                     </div>
+
+                    <?php 
+                        $items = [
+                            [
+                                'title' => 'Privacy',
+                                'content' => 'Your privacy is important to us at SmartAdmin and the protection, confidentiality and integrity of your personal data are our prime concerns.
+                                            We will only use your personal information to administer your account, provide the products and services you have requested from us, and to keep you informed about our products and services (if you have consented to this).
+                                            We only use your data for the purposes for which it was collected and, where relevant, to meet local legal obligations.
+                                            We will retain your personal information only for as long as is necessary for the purposes for which the information was collected, or as long as is required pursuant to law.'
+                            ],
+                            [
+                                'title' => 'Cookies and other similar technologies',
+                                'content' => 'We collect certain data through cookies and similar technologies (e.g. web beacons, tags, device identifiers). Cookies are text files placed on your computer to collect standard internet log information and visitor behaviour information. This information is used to track visitor use of the website and to compile statistical reports on website activity. We register your interaction with our services in order to improve our website, content and services. Our use of such technologies and the data collected is described in more detail in our Cookie Policy. You can manage your cookie preferences through your browser settings.'
+                            ],
+                            [
+                                'title' => 'Test title',
+                                'content' => 'Test content'
+                            ]
+                        ];
+                    ?>
+
                     <div class="panel-container show">
                         <div class="panel-content">
-                            <div class="panel-content">
-                                <div class="bg-warning-100 border border-warning rounded">
-                                    <div class="input-group p-2 mb-0">
-                                        <input type="text" class="form-control form-control-lg shadow-inset-2 bg-warning-50 border-warning" id="js-list-msg-filter" placeholder="Filter list">
-                                        <div class="input-group-append">
-                                            <div class="input-group-text bg-warning-500 border-warning">
-                                                <i class="fal fa-search fs-xl"></i>
-                                            </div>
-                                        </div>
-                                    </div>
+                            <div class="fs-lg fw-300 p-5 bg-white border-faded rounded mb-g">
 
-                                    <?php
-                                        
-                                        $items = [
-                                            [
-                                                "title" => "Reports",
-                                                "tags" => "reports file"
-                                            ],
+                            <?php foreach($items as $item):?>
+                                <h2><?= $item['title'];?></h2>
+                                <p class="mb-g"><?= $item['content'];?></p>
+                            <?php endforeach;?>
 
-                                            [
-                                                "title" => "Analytics",
-                                                "tags" => "analytics graphs"
-                                            ],
-
-                                            [
-                                                "title" => "Export",
-                                                "tags" => "export download"
-                                            ],
-
-                                            [
-                                                "title" => "Storage",
-                                                "tags" => "storage"
-                                            ],
-                                            [
-                                                "title" => "Test",
-                                                "tags" => "test sts"
-                                            ]
-                                        ];
-
-                                    ?>
-
-                                    
-                                    <ul id="js-list-msg" class="list-group px-2 pb-2 js-list-filter">
-                                        <?php foreach($items as $item):?>
-                                            <li class="list-group-item">
-                                                <span data-filter-tags="<?= $item['tags'];?>"><?= $item['title'];?></span>
-                                            </li>
-                                        <?php endforeach;?>
-                                    </ul>
-                                    <div class="filter-message js-filter-message mt-0 fs-sm"></div>
-                                </div>
                             </div>
                         </div>
                     </div>
